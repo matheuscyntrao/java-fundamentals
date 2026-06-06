c0e98a9ae0293c193454f8cc79a26553b5658ac4
cyntrao <matheus.cyntrao@gmail.com>
2026-06-06 18:23:09 -0300

    SimpleJavaProgram

---
+++ examples/src/SimpleJavaProgram.java
@@ -0,0 +1,17 @@
+/*
+    Java is case-sensitive.
+    The correct pattern to name classes is CamelCase.
+    The class names must start with a upperCase letter and have no limitations of characters.
+    Static classes means that the class do not operate/create any object.
+    Method must use lowerCase at the first character.
+    Public is an access modifier, these modifiers control the level of access to the other parts of a program.
+    Everything in a Java program must be inside a class.
+ */
+
+public class SimpleJavaProgram {
+
+    public static void main(String[] args) {
+        System.out.println("We will not use 'Hello, World!'");
+    }
+
+}
