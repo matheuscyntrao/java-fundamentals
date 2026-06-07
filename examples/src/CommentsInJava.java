ce17c6a0cb4bf58ebb11349c5cf81de4b346a880
cyntrao <matheus.cyntrao@gmail.com>
2026-06-07 14:16:46 -0300

    CommentsInJava

---
+++ examples/src/CommentsInJava.java
@@ -0,0 +1,11 @@
+public class CommentsInJava {
+
+    // Most used
+    /* Longer comments*/
+    /**
+     * A short description what the block of code does.
+     * @version
+     * @author
+     */
+
+}
