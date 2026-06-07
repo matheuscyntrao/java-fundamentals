daef03c4f840039d20dee6cbce19ff90406177c8
cyntrao <matheus.cyntrao@gmail.com>
2026-06-07 13:37:26 -0300

    BugReports JVM and a little of fun history that enforces my point.

---
+++ examples/src/JavaLanguageSpecification.java
@@ -0,0 +1,19 @@
+public class JavaLanguageSpecification {
+
+    /* http://docs.oracle.com/javase/specs.
+
+       Famous bug closed as it will be not fixed.
+       http://bugs.java.com/bugdatabase/index.jsp ID 4252539
+
+       A sun engineer added an explanation that the JVM does not mandate that main is public and the fixing it will cause
+       potential troubles.
+
+       http://docs.oracle/javase/specs/jvms/se8/html
+
+       After the java laucher 1.4 the "sanity" came and the enforces the main method to be public.
+
+       On the one hand, it is frustrating to have quality assurance engineers, who are often overworked and not always experts in the fine points of Java.
+       (this is from a Sun engineers, but it is my opinion as well haha).
+
+     */
+}
