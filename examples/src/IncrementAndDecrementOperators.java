347d79fb7aafd79c59cfce46943b1b2e38293c30
cyntrao <matheus.cyntrao@gmail.com>
2026-06-10 11:37:34 -0300

    IncrementAndDecrement

---
+++ examples/src/IncrementAndDecrementOperators.java
@@ -0,0 +1,18 @@
+public class IncrementAndDecrementOperators {
+
+    static void main() {
+        int n = 12;
+        n++;
+
+        int m = 7;
+        int o = 7;
+        int a = 2 * ++m; // It is not a Java best practice because leads confusing code, but it works.
+        int b = 2 * o++;
+
+        System.out.println(a + "/" + b);
+
+        System.out.println(a + --m);
+        System.out.println(b + o--);
+    }
+
+}
