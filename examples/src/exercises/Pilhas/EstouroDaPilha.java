6d64a07f98543bc7f7b8f2e6097b7aa6d5877545
cyntrao <matheus.cyntrao@gmail.com>
2026-06-17 22:18:35 -0300

    Estouro de pilhas memória Stack

---
+++ examples/src/exercises/Pilhas/EstouroDaPilha.java
@@ -0,0 +1,26 @@
+package exercises.Pilhas;
+
+public class EstouroDaPilha {
+
+    private static long contadorFrames = 0;
+
+    public static void main(String[] args) {
+        System.out.println("MEMÓRIA STACK");
+
+        try {
+            metodoRecursivoInflood(100, 200, 300);
+        } catch (StackOverflowError e) {
+            System.out.println(e);
+            System.out.printf("A Stack da sua Thread principal estourou após empilhar %,d frames!%n", contadorFrames);
+        }
+    }
+
+    private static void metodoRecursivoInflood(int param1, int param2, int param3) {
+        contadorFrames++;
+
+        long variavelLocal1 = contadorFrames;
+        long variavelLocal2 = variavelLocal1 * 2;
+
+        metodoRecursivoInflood(param1, param2, param3);
+    }
+}
