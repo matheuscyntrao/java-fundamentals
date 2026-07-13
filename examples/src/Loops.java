534d248721d56c4ad32676ec517569ebe0f1e2dd
cyntrao <matheus.cyntrao@gmail.com>
2026-07-13 20:24:04 -0300

    Retirement loop 2.

--- examples/src/Loops.java
+++ examples/src/Loops.java
@@ -54,4 +54,34 @@ public class Loops {
 
     }
 
+    public static void Retirement2() {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("How much money will you contribute every year?");
+        double payment = in.nextDouble();
+
+        System.out.println("Interest rate in %:");
+        double interestRate = in.nextDouble();
+
+        double balance = 0;
+        int year = 0;
+
+        java.lang.String input;
+
+        do {
+            balance += payment;
+            double interest = balance * interestRate/100;
+            balance += interest;
+
+            year++;
+
+            System.out.printf("After year %d, your balance is %, .2f%n", year, balance);
+
+            System.out.println("Ready to retire? (Y/N) ");
+            input = in.next();
+
+        } while(input.equals("N"));
+
+    }
+
 }
