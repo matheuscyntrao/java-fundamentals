13f1b432dbd9c2999227128ee2c24c47275b4991
cyntrao <matheus.cyntrao@gmail.com>
2026-07-15 23:38:50 -0300

    Multiple Selections

---
+++ examples/src/MultipleSelections.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class MultipleSelections {
+
+    static void main() {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Select an option (1, 2, 3, 4)");
+        int choice = in.nextInt();
+
+        switch(choice) {
+            case 1: break;
+            case 2: break;
+            case 3: break;
+            case 4: break;
+            default: break;
+        }
+    }
+
+}
