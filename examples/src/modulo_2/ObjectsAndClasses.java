edaba2c9feeca9403f16875aadd9ddb185815b1c
cyntrao <matheus.cyntrao@gmail.com>
2026-08-01 11:58:03 -0300

    Objects

---
+++ examples/src/modulo_2/ObjectsAndClasses.java
@@ -0,0 +1,53 @@
+package modulo_2;
+
+public class ObjectsAndClasses {
+
+    // Object-oriented programming requires a different way of thinking
+    static void main() {
+        // Replacement of structured or procedural languages (paradigm) (1970)
+        // Made by objects, each object has a specific functionality exposed and hidden implementation (algorithms)
+        // Off-the-shelf / Custom designed
+        // Always depends on your budget or time.
+        // Pascal Niklaus Wirth
+        // First Algorithms and then Data Structures
+        // OOP Data Structures firts, then functionality
+        // Maded for complex and big problems
+        // Easier to grasp
+        // Easier to find bugs (OOP vs Procedural)
+
+
+        // Procedure1 -> Global data
+        // Procedure2    Global data
+
+        // OOP to DDD
+
+        // Method1 -> Object Data1
+        // Method2 -> Object Data1
+
+        // A class is the template or blueprint from which objects are made.
+        // Think of classes as cookie cutters. Objects are the cookies themselves.
+        // The creation of an object is called Instance
+
+        // Java library supplies several thousand classes for such diverse purposes
+        /*
+            - interface design
+            - dates and calendars
+            - network programming
+         */
+
+        // Encapsulation (information hidding) is a key concept in working with objects.
+        // Formally, encapsulation is simply combining data and behavior in one package and hiding the implementation details from the users of the object
+        // The bits of data in an object are called its Instance Field, and the procedures that operate on the data are called its methods.
+        // The set of those values is the current State of the object
+        // It may change after some method are invoked
+
+        // the key to making encapsulation work is to have methods NEVER directly acces instance fields in a class other than their own.
+        // Programs should interact with object data ONLY thourgh the object's methods.
+        // Reuse and reliability.
+        // Classes can be build Extending other classes. (Cosmic Superclass - Object) Inheritance
+        // All other classes extends of this class in Java.
+
+    }
+
+
+}
