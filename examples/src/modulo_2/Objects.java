f73d324388d2dae1d783e89374fecea987524141
cyntrao <matheus.cyntrao@gmail.com>
2026-08-03 10:21:38 -0300

    Objects Annonations

---
+++ examples/src/modulo_2/Objects.java
@@ -0,0 +1,16 @@
+package modulo_2;
+
+public class Objects {
+
+    // You should be able to identify three key characteristics of objects
+    // Behavior - What can you do with this object or what methods can you apply to it (methods you call)
+    // The objects state - how does the object react when you invoke those methods? (what the methods do)
+        // Can change when you interact with but not spontaneously
+        // If the object changes without a method call someone broke encapsulation
+        // Influences rirectly with objects behavior
+        // Order > Shipped > Paid > Delivered
+    // Objects identity - how is the object distinguished from others that may have the same behavior and state?
+        // Identifier (SQL id, UUID transactions, HashCode equals)
+        // Differ AWAYS identity and USUALLY differ in their state
+
+}
