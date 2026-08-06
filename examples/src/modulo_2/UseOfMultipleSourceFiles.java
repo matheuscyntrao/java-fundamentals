9ef922cff972feba11a9cb8cc88d30010e44c6b2
cyntrao <matheus.cyntrao@gmail.com>
2026-08-06 08:21:04 -0300

    Constructors

---
+++ examples/src/modulo_2/UseOfMultipleSourceFiles.java
@@ -0,0 +1,30 @@
+package modulo_2;
+
+import java.time.LocalDate;
+
+public class UseOfMultipleSourceFiles {
+
+    // Always separte files when you work with OOP
+    // You can compile the class and classTest using wildcard (*) at the terminal
+    // javac Employee*.java
+
+    // Dissecting the Employee Class
+
+    //public Employe(String n, double s, int year, int month, int day)
+    //public String name()
+    //public double getSalary()
+    //public LocalDate getHireDay()
+    //public void raiseSalary(double byPercent)
+
+    // First steps with Constructors
+    // The constructor is the same as the name of the class.
+    // This constructor runs when you construct object
+    // Giving the instance fields the initial state you want them to have
+    // Only called in conjunction with the new operator
+    // You can't apply a constructor to an existing oject to reset the instance fields
+    // Constructors can take zero, one, or more parameters
+    // A class can have more than one constructor (polimorphism)
+
+
+
+}
