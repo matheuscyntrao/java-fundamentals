d9ef201ce927b6c70fe2fad0c03e54389551e000
cyntrao <matheus.cyntrao@gmail.com>
2026-08-07 11:22:08 -0300

    Private Methods vs Public.

---
+++ examples/src/modulo_2/PrivateMethods.java
@@ -0,0 +1,12 @@
+package modulo_2;
+
+public class PrivateMethods {
+
+    // When implementing a class, we make all data fields private because public data are dengerous.
+    // And how about methods?
+    // Keep special protocol or calling order private outside public interface
+    // By making you method private, you are under no obligation to keep ir available if you chanfe your implementation.
+    // The method may well be harder to implement or unnecessary if the data representation changes; this is relevant.
+    // If you have private methods, you can simply drop ip, public may be used for another part of the program
+
+}
