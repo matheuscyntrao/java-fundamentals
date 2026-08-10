3ed23fad87561aeb9a84204a2cc0017a9a8b6585
cyntrao <matheus.cyntrao@gmail.com>
2026-08-09 21:11:26 -0300

    The main methods

---
+++ examples/src/modulo_2/TheMainMethod.java
@@ -0,0 +1,18 @@
+package modulo_2;
+
+public class TheMainMethod {
+
+    // Notethat you can callstatic methods witout having any objects.
+    // For example,you never construct any objects of the Math class to call Math.pow.
+
+    // For the same reason, the main method is a static method
+
+    public static void main (String[] args) {
+        // construct objects here
+    }
+
+    // The main method does not operate on any objects.
+    // In fact, when a program starts, there aren't any objects yet.
+    // The satic main method executes, and constructs the objects that the program needs.
+
+}
