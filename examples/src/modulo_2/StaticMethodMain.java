8a4208b3690964bb43a35da31e9ee186826dca62
cyntrao <matheus.cyntrao@gmail.com>
2026-08-12 10:18:27 -0300

    Static Main Method.

---
+++ examples/src/modulo_2/StaticMethodMain.java
@@ -0,0 +1,17 @@
+package modulo_2;
+
+public class StaticMethodMain {
+
+    // static <T> void requireNonNull(T object)
+    // static <T> void requiteNonNull(T object, String message)
+    // static <T> void requireNonNull(T object, Supplier<String messageSuplier>
+    /*
+        If object is null, these methods throw a NullPointerException with no message or the given message.
+     */
+    // static <T> T requireNonNullElse(T object, T defaultObject)
+    // static <T> T requireNonNullElseGet(T object, Supplier<T> defaultSuplier)
+    /*
+        Returns object if it is not null, or the default object if object is null.
+     */
+
+}
