a843eae839daf5a8754b6e4adc0e9d50bceb6ea7
cyntrao <matheus.cyntrao@gmail.com>
2026-08-14 09:39:38 -0300

    Local variable and Modifying object parameters.

--- examples/src/modulo_2/MethodParameters.java
+++ examples/src/modulo_2/MethodParameters.java
@@ -20,6 +20,8 @@ public class MethodParameters {
         // x is trippled- it is now 30. but percent is still 10
         // The method ends, and the parameter variable x is no longe in use.
         tripleValue(percent);
+        main2();
+        main3();
     }
     // No matter how the method is implemented, we know that after the method call, the value of percent is still 10
     // Let us look a tilttle more closely at this situation.
@@ -38,7 +40,8 @@ public class MethodParameters {
 
     static void main2() {
         WorkingWithNullReferences.Employee2 harry = new WorkingWithNullReferences.Employee2();
-        harry.raiseSalary(200);
+        harry.raiseSalary(2000);
+        System.out.println(harry.string + "" + harry.s + "" + harry.salary);
     }
 
     // x is initialized with a copy of the value of harry - that is, an object reference
@@ -47,4 +50,32 @@ public class MethodParameters {
     // continues to refer to the object whose salary was tripled.
     // The method gets a copy of the object reference, and both the original and the copy refer to the same object.
     // Java doesn't call by reference for objects. THIS IS FALSE. (book authors have been saying shit haha)
+
+    // Let's try to write a method that swaps two String objects:
+    public static void swap(String x, String y) {
+        String temp = x;
+        x = y;
+        y = temp;
+        System.out.println(x + " " +y);
+    }
+
+    // If Java used call by reference for objects, this method would work:
+    static void main3() {
+        var a = new String("a");
+        var b = new String("b");
+
+        swap(a, b);
+        // However, the method doest not actually change the object references that are stored in the variables a and b.
+        // The x and y parameters of the swap method are initialized with copies of these references.
+        // The method then proceeds to swap these copies.
+        System.out.println(a + " " + b);
+    }
+
+    // A method cannotmodify a parameter of a primitive type (that is, numbers or boolean values).
+    // A method can change the state of an object parameter.
+    // A method cannot make an object parameter refer to a new object.
+
+
+
+
 }
