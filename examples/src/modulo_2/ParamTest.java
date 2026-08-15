62f859cc4005a7df3a519d2713f4bcdde8fac505
cyntrao <matheus.cyntrao@gmail.com>
2026-08-14 21:58:01 -0300

    Param Tests (Values and Objects)

---
+++ examples/src/modulo_2/ParamTest.java
@@ -0,0 +1,75 @@
+package modulo_2;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class ParamTest {
+
+    public static void main(String[] args) {
+        // Test 1 : Methods can't modify numeric parameters
+        System.out.println("Testing trippleValue");
+        double percent = 10;
+        System.out.println("Before percent = " + percent);
+        trippleValue(percent);
+        System.out.println("After percent = " + percent);
+
+
+        // Test 2: Method can change the state of object parameters
+        System.out.println("Testing trippleSalary:");
+        var harry = new Employee("Harry", 50000);
+        System.out.println("Before salary = " + harry.getSalary());
+        trippleSalary(harry);
+        System.out.println("After salary = " + harry.getSalary());
+
+        // Test 3: Methods can't attach new objects to object parameters
+        System.out.println("Testing swap");
+        var a = new Employee("Alice", 7000);
+        var b = new Employee("Bob", 6000);
+        System.out.println("Before a = " + a.getName());
+        System.out.println("Before b = " + b.getName());
+        swap(a, b);
+        System.out.println("After a = " + a.getName());
+        System.out.println("After b = " + b.getName());
+
+    }
+
+    public static void trippleValue(double x) {
+        x = 3 * x;
+    }
+
+    public static void trippleSalary(Employee x) {
+        x.raiseSalary(200);
+        System.out.println("End of method: salary = " + x.getSalary());
+    }
+
+    public static void swap(Employee x, Employee y) {
+        Employee temp = x;
+        x = y;
+        y = temp;
+        System.out.println("End of method x = " + x.getName());
+        System.out.println("End of method y = " + y.getName());
+    }
+}
+
+class Employee{
+    private String name;
+    private double salary;
+
+    public Employee(String n, double s) {
+        name = n;
+        salary = s;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void raiseSalary(double byPercent) {
+        double raise = salary * byPercent / 100;
+        salary += raise;
+    }
+
+}
