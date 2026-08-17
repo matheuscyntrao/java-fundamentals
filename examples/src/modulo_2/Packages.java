dcec7ad55ad6eb7753c09720aa022ade9010682c
cyntrao <matheus.cyntrao@gmail.com>
2026-08-17 10:26:48 -0300

    Packages

---
+++ examples/src/modulo_2/Packages.java
@@ -0,0 +1,14 @@
+package modulo_2;
+
+public class Packages {
+
+    // Java allows you to group classes in a collection called a package.
+    // Packages are convenient for organizing your work and for separating yourwork from code libraries provided by others.
+
+    // Guarantee the uniqueness of a class names.
+    // If are two classes with the same name (two programmers decided to build) there's no conflict.
+    // In fact, to absolutely guarantee a unique package use an Internet domain name com.horstmann.corejava.Employee
+
+    // java.util and java.util.jar have nothing to do with each other.
+
+}
