658949dc8ddee6edb45976c2a3408313c42beed3
cyntrao <matheus.cyntrao@gmail.com>
2026-08-20 19:32:56 -0300

    Packages.

---
+++ examples/src/modulo_2/AdditionOfClassIntoPackage.java
@@ -0,0 +1,13 @@
+package modulo_2;
+
+public class AdditionOfClassIntoPackage {
+
+    // To place classes inside a package, put the name of the package at the top of your source  file.
+    // Before the code that defines the classes in the package.
+
+    // If you don'tput a package statement in the source file, then the classes in that source
+    // then the source file belong to the unnamed package.
+
+    // You must compile and run classes from the base directory.
+
+}
