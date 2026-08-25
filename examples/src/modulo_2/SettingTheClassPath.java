0f8de7ed54b0c843870f8ffcbdfbe8de5edcf890
cyntrao <matheus.cyntrao@gmail.com>
2026-08-25 11:00:47 -0300

    Setting the class path.

---
+++ examples/src/modulo_2/SettingTheClassPath.java
@@ -0,0 +1,20 @@
+package modulo_2;
+
+public class SettingTheClassPath {
+
+    // Specify the class path with the option -classpath (or -cp, --class-path)
+    // java 0classpath /home/user/classdir:.:/home/user/archives/archive.jar MyProg
+    // java -classpath c:\home'\'user\classdir;.;c:\archives\archive.jar MyProg
+
+    // The entire command must be typed onto a single line.
+    // It is a good idea to place such a long command line into a shell script or a batch file
+
+    // Using the -classpath option is the preferred approach for setting the class path.
+    // An alternate approach is the CLASSPATH environment variable.
+    // The details depende on your shell.
+    // export CLASSPATH=/home/user/classdir:.:/home/user/archives/archive.jar
+    // set CLASSPATH=c:\classdir;.;c:\archives\archive.jar
+
+
+
+}
