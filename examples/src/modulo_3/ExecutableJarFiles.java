08ea91d8bd681d7cac9e921b306ad19edfd876d8
cyntrao <matheus.cyntrao@gmail.com>
2026-08-27 20:23:27 -0300

    Executable JAR Files.

---
+++ examples/src/modulo_3/ExecutableJarFiles.java
@@ -0,0 +1,13 @@
+package modulo_3;
+
+public class ExecutableJarFiles {
+
+    // You can use the e option of the jar command to specify the entry point of your program
+    // jar cvfe MyProgram.jar com.mycompany.mkpkg.MainAppClass file1.class file2.class Main-Class: com.mycompany.mypkg.MainAppClas
+    // On Windows the Java Reuntine installer creates a file association for the ".jar" extension that launches the file with the javaw -jar comand. (don't open the shell window)
+    // On Mac OS X, the operating system recognizes the ".jar" file extension and executes the Java proram when you double-click a JAR file.
+
+    // Java program in JAR file does not have the same feel as a native application. On Windows, you can use third-party wrapper utilities.
+    // That turn JAR files into Windows executables.
+
+}
