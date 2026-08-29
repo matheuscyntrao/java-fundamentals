85de4f9fab17da6c5e9394b2fa8833a1e328d272
cyntrao <matheus.cyntrao@gmail.com>
2026-08-29 15:26:28 -0300

    Command Line Annotations

---
+++ examples/src/modulo_3/NoteAboutCommandLine.java
@@ -0,0 +1,20 @@
+package modulo_3;
+
+public class NoteAboutCommandLine {
+
+    // The options of commands in the Java Development Kit have traditionally used single fashes followed by multiletter options
+    // java -jar
+    // javac -Xlint:unchecked -classpath
+    // ls --human-readable
+    // ls -h
+    // javac --class-path /home/user/classdir
+    // javac --class-path=/home/user/classdir
+    // javac -p moduledir
+    // javac -pmoduledir
+    // jar -cvf MyProgram.jar -e mypackae.Myprogram */*.class
+    // This has created a muddle that will hopefully get cleaned up over time.
+    // As much as we'd like to move away fromthe archaic jar options, it seems best to wait until the dust has settled.
+    // jar --create --verbose --file jarFileName file1, file2
+    // jar -c -v -f jarFileName file1, file2
+
+}
