b2c3163bbdf906639418b7b517d9ab411afca69b
cyntrao <matheus.cyntrao@gmail.com>
2026-08-29 16:26:17 -0300

    Class comments

---
+++ examples/src/modulo_3/ClassComments.java
@@ -0,0 +1,16 @@
+package modulo_3;
+
+public class ClassComments {
+
+    // The class commment must be placed after any import statements, directly before the class definition.
+    /**
+     * A {@code Card} object represents a playing car, such
+     * as "Queen of Hearts". A card has a suit (Diamond, Heart, Space or Club)
+     * and a value (1 = Ace, 11 = Jack, 12 = Queen)
+     *
+     */
+    public class Card {
+
+    }
+
+}
