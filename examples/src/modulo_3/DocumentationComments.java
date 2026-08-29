d40d36a5d99073a2bd0b9643e8319d1f82d262f6
cyntrao <matheus.cyntrao@gmail.com>
2026-08-29 16:23:18 -0300

    Comment Insertion

---
+++ examples/src/modulo_3/DocumentationComments.java
@@ -0,0 +1,27 @@
+package modulo_3;
+
+public class DocumentationComments {
+
+    // Javadoc, generates HTML documentation form your source files.
+    // If you add comments that start with the special delimiter /** to your source code, you too can easily produce
+    // Professional-looking documentation.
+    // This is a very nice approach because its lets you keep your code and documentation probably know, the code and comments tend to diverge over time.
+
+    // Comment Insertion
+    /*
+    - Modules
+    - Packages
+    - Public classes and interfaces
+    - Public and protected fields
+    - Public and protected constructors and methods.
+     */
+
+    // You can supply a comment for each of these features.
+    // Each comment is placed immediately above the feature it describes.
+    // A comment starts with a /** and ends with a */
+
+    // In the free-form text, you can use HTML modifiers such as <em> </em> for emphasis <strong> </strong>
+    // <ul>/<li> for bulleted lists and <img> to include an image.
+    // To hype monospaced code, use {@code} instead of <code></code> then you don't have to worry about escaping characters inside code.
+
+}
