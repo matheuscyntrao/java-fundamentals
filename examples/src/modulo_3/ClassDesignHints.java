92ca26b2aecc6fcafd6544ae753717504c4e980a
cyntrao <matheus.cyntrao@gmail.com>
2026-08-31 16:49:24 -0300

    Class Design Hints

---
+++ examples/src/modulo_3/ClassDesignHints.java
@@ -0,0 +1,15 @@
+package modulo_3;
+
+public class ClassDesignHints {
+
+    /**
+     * Always keep data private
+     * Always initialize data
+     * Don't use too many basic types in a class
+     * Not all fields need individual field accessors and mutators
+     * Break up classes that have too many responsibilities (Solid) - Single Responsability
+     * Make the names of your classes and methods reflect their responsabilities
+     * Prefer immutable classes
+     */
+
+}
