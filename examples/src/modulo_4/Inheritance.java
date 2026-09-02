853a757e7ead0e9aaa90b0a2014c7a47656695db
cyntrao <matheus.cyntrao@gmail.com>
2026-09-01 22:57:29 -0300

    Inheritance Introduction.

---
+++ examples/src/modulo_4/Inheritance.java
@@ -0,0 +1,12 @@
+package modulo_4;
+
+public class Inheritance {
+
+    // The main idea is that you can create new classes that are built on existing classes.
+    // When you inheritfrom an existing class, you reuse (or inherit) its methods, and you can add new method and fields
+    // to adapt your new class to new situations.
+    // This technique is essential in Java programming.
+
+    // Reflection is a powerful feature, but it is undeniable complex.
+
+}
