c837aedf04c05c62cf8655dcaa93236d0ea1b4b3
cyntrao <matheus.cyntrao@gmail.com>
2026-09-17 14:01:49 -0300

    fix: pegando parent dir / createDirectories

--- examples/src/br/com/exercisesdio/exercise6/Formatacao/Files/File.java
+++ examples/src/br/com/exercisesdio/exercise6/Formatacao/Files/File.java
@@ -2,19 +2,27 @@ package br.com.exercisesdio.exercise6.Formatacao.Files;
 
 import br.com.exercisesdio.exercise6.Formatacao.FileManager;
 
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 
 public abstract class File implements FileManager {
 
     private final String userDir = System.getProperty("user.dir");
-    private final String filesDir = "files";
     private final String fileName;
 
     public File(String fileName) {
         this.fileName = fileName;
+        try {
+            Files.createDirectories(this.getFullDir().getParent());
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public Path getFullDir() {
+        String filesDir = "files";
         return Path.of(userDir, filesDir, this.getFileName());
     }
 
